ed97885042ede2e9f2593ede141235cd9567a9c5
Quirax Lee <quiraxical@gmail.com>
2024-06-26 16:50:44 +0900

    Modified prettier settings

--- src/pages/Print/Print.tsx
+++ src/pages/Print/Print.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 import './Print.css';
 
 const Print: React.FC = () => {
-  return (
-    <div className="container">
-      <h1>Print Screen</h1>
-    </div>
-  );
+    return (
+        <div className="container">
+            <h1>Print Screen</h1>
+        </div>
+    );
 };
 
 export default Print;
