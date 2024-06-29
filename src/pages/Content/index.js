a21bda864751ab351dc1775842da8e5887486d9b
Quirax Lee <quiraxical@gmail.com>
2024-06-30 00:59:45 +0900

    Make the extension observe location.href because Band is SPA

--- src/pages/Content/index.js
+++ src/pages/Content/index.js
@@ -1,6 +1,37 @@
 import { printLine } from './modules/print';
 
-console.log('Content script works!');
-console.log('Must reload extension for modifications to take effect.');
+// console.log('Content script works!');
+// console.log('Must reload extension for modifications to take effect.');
 
-printLine("Using the 'printLine' function from the Print Module");
+// printLine("Using the 'printLine' function from the Print Module");
+
+const onEnterBandPage = () => {
+    // 밴드 페이지에 들어갔을 때
+};
+
+const onLeaveBandPage = () => {
+    // 밴드 페이지에서 벗어났을 때 (e.g. 밴드 목록 페이지)
+};
+
+const isBandPage = (href) => {
+    if (href.match(/band.us\/band\//)) onEnterBandPage();
+    else onLeaveBandPage();
+};
+
+// ref: https://stackoverflow.com/a/46428962
+const observeUrlChange = () => {
+    let oldHref = document.location.href;
+    const body = document.querySelector('body');
+    const observer = new MutationObserver((mutations) => {
+        if (oldHref !== document.location.href) {
+            oldHref = document.location.href;
+            /* Changed ! your code here */
+            isBandPage(oldHref);
+        }
+    });
+    observer.observe(body, { childList: true, subtree: true });
+
+    isBandPage(oldHref);
+};
+
+window.onload = observeUrlChange;
