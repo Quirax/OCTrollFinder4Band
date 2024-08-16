5298efc28b2193e09265ed9728269c320cbd6a8e
Quirax Lee <quiraxical@gmail.com>
2024-08-16 15:38:36 +0900

    Set default values of date constraints

--- src/pages/Popup/Criteria.jsx
+++ src/pages/Popup/Criteria.jsx
@@ -29,11 +29,35 @@ const Criteria = ({ label, option }) => {
     );
 };
 
+// ref: https://velog.io/@rkio/Javascript-YYYY-MM-DD-%ED%98%95%ED%83%9C%EC%9D%98-%EB%82%A0%EC%A7%9C-%EC%A0%95%EB%B3%B4%EB%A5%BC-%EB%A7%8C%EB%93%A4%EC%96%B4%EB%B3%B4%EC%9E%90
+const formatDate = (date) =>
+    `${date.getFullYear()}-${(date.getMonth() + 1)
+        .toString()
+        .padStart(2, '0')}-${date.getDate().toString().padStart(2, '0')}`;
+
 export const DateConstraints = () => {
+    const [dateStart, setDateStart] = useState(formatDate(new Date()));
+    const [dateEnd, setDateEnd] = useState(formatDate(new Date()));
+
     const Option = (isEnabled) => (
         <>
-            기간: <input type="date" id="date_start" disabled={!isEnabled} /> ~{' '}
-            <input type="date" id="date_end" disabled={!isEnabled} />
+            기간:{' '}
+            <input
+                type="date"
+                id="date_start"
+                disabled={!isEnabled}
+                value={dateStart}
+                onChange={(e) => setDateStart(e.target.value)}
+            />
+            {' ~ '}
+            <input
+                type="date"
+                id="date_end"
+                disabled={!isEnabled}
+                value={dateEnd}
+                min={dateStart}
+                onChange={(e) => setDateEnd(e.target.value)}
+            />
         </>
     );
 
