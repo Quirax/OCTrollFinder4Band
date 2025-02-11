661153dfe9a14684e1a88a2e555a2b373e11fd4c
Quirax Lee <quiraxical@gmail.com>
2025-02-11 18:07:35 +0900

    Added graph view demo

--- src/pages/Stat/StatView/PeerMentions.jsx
+++ src/pages/Stat/StatView/PeerMentions.jsx
@@ -1,5 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { createStatView, SeriesType } from './AbstractStatView';
+import Graph from 'react-graph-vis';
+import { createGlobalStyle } from 'styled-components';
+
+const GraphStyle = createGlobalStyle`
+    div.vis-tooltip {
+        position: absolute;
+        color: black;
+        border: 1px solid black;
+        background-color: white;
+    }
+`;
 
 /**
  * @type {import('./AbstractStatView').StatView}
@@ -27,7 +38,69 @@ export const PeerMentions = createStatView(
                 stackId: 1,
             },
         ],
-        ChartElement: ({ $chartData, $chartOptions, $criteria }) => <></>,
+        ChartElement: ({ $chartData, $chartOptions, $criteria }) => {
+            const graph = {
+                nodes: [
+                    { id: 1, label: 'Node 1', title: 'node 1 tootip text' },
+                    { id: 2, label: 'Node 2', title: 'node 2 tootip text' },
+                    { id: 3, label: 'Node 3', title: 'node 3 tootip text' },
+                    { id: 4, label: 'Node 4', title: 'node 4 tootip text' },
+                    { id: 5, label: 'Node 5', title: 'node 5 tootip text' },
+                ],
+                edges: [
+                    { from: 1, to: 2, value: 1, title: 'absolute' },
+                    { from: 1, to: 3, value: 3 },
+                    { from: 2, to: 4, value: 5 },
+                    { from: 2, to: 5, value: 7 },
+                ],
+            };
+
+            const options = {
+                layout: {
+                    hierarchical: true,
+                },
+                edges: {
+                    color: '#000000',
+                },
+            };
+
+            const events = {
+                select: function (event) {
+                    var { nodes, edges } = event;
+                    console.debug('On select node', event);
+                },
+            };
+
+            let resizeGraph;
+
+            useEffect(() => {
+                return () => window.removeEventListener('resize', resizeGraph);
+            });
+
+            return (
+                <>
+                    <GraphStyle />
+                    <Graph
+                        graph={graph}
+                        options={options}
+                        events={events}
+                        getNetwork={(network) => {
+                            const container = network.view.body.container;
+                            console.debug('Graph container size', container.clientWidth, container.clientHeight);
+
+                            resizeGraph = () => {
+                                network.setSize(0, 0);
+                                console.debug('Graph container size', container.clientWidth, container.clientHeight);
+                                network.setSize(container.clientWidth, container.clientHeight);
+                                network.fit();
+                            };
+
+                            window.addEventListener('resize', resizeGraph);
+                        }}
+                    />
+                </>
+            );
+        },
     },
     (data, criteria) =>
         Object.values(
